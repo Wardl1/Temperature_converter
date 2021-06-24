c73fc1759f2062a6e5ec3d16e3b6dcc0f433f70f
Wardl1 <66809651+Wardl1@users.noreply.github.com>
2021-06-25 11:20:23 +1200

    Update 01_Help_GUIv1.py

--- 01_Help_GUIv1.py
+++ 01_Help_GUIv1.py
@@ -6,13 +6,25 @@ class Convertor:
     def __init__(self):
         print("hello world")
 
-    # Formatting variables...
-    background_color = "light blue"
+        # Formatting variables...
+        background_color = "light blue"
+
+        # Convertor Main Screen GUI..
+        self.convertor_frame = Frame(width=300, height=300, bg=background_color)
+        self.convertor_frame.grid()
+
+        # Temperature Conversion Heading (row 0)
+        self.temp_convertor_label = Label(text="Temperature Convertor",
+                                          font=("Arial", "16", "bold"),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_convertor_label.grid(row=0)
+
 
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
     root.title("Temperature Convertor")
-    something = Convertor(root)
+    something = Convertor()
     root.mainloop()
