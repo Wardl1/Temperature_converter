08a67aea4b3e354fc417ca2d48066f9f4c26ea63
Wardl1 <66809651+Wardl1@users.noreply.github.com>
2021-06-27 20:35:53 +1200

    Update 01_Help_GUIv3.py

--- 01_Help_GUIv3.py
+++ 01_Help_GUIv3.py
@@ -35,15 +35,31 @@ class Help:
     def __init__(self):
         background = "orange"
 
+        # sets up child window (ie: help box)
+        self.help_box = Toplevel()
 
-    # set up GUI Frame
+        # set up GUI Frame
+        self.help_frame = Frame(self.help_box, width=300, bg=background)
+        self.help_frame.grid()
 
-    # Set up Help heading (row 0)
+        # Set up Help heading (row 0)
+        self.how_heading = Label(self.help_frame, text="Help/Instructions",
+                                 font="arial 10 bold", bg=background)
+        self.how_heading.grid(row=0)
 
-    # Help text (label, row 1)
+        # Help text (label, row 1)
+        self.help_text = Label(self.help_frame, text="", justify=LEFT,
+                               width=40, bg=background, wrap=250)
+        self.help_text.grid(row=1)
 
-    # Dismiss button (row 2)
+        # Dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss",
+                                  width=10, bg="orange", font="arial 10 bold",
+                                  command=self.close_help)
+        self.dismiss_btn.grid(row=2, pady=10)
 
+    def close_help(self):
+        self.help_box.destroy()
 
 # main routine
 if __name__ == "__main__":
