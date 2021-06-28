4c4f586e213353ff10545ef041d00b2e13ee5d2f
Wardl1 <66809651+Wardl1@users.noreply.github.com>
2021-06-29 11:57:32 +1200

    Create 03_Celsuis_to_Fahrenheit.py

---
+++ 03_Celsuis_to_Fahrenheit.py
@@ -0,0 +1,21 @@
+""" Converting Celsius to Fahrenheit v1
+Converting from degrees Celsuis to Fahrenheit
+Function takes in a value, does the conversion and puts the answer into a list
+"""
+
+
+def to_f(from_c):
+    fahrenheit = (from_c * 9 / 50 +32)
+    return fahrenheit
+
+
+# Main Routine
+temperatures = [0, 40, 100]
+converted = []
+
+for item in temperatures:
+    answer = to_f(item)
+    ans_statement = "{} degrees c is {} degrees F".format(item, answer)
+    converted.append(ans_statement)
+
+print(converted)
