c6362a48b9d15f85e75c5a396aea38ee57ef813c
Wardl1 <66809651+Wardl1@users.noreply.github.com>
2021-07-08 14:19:19 +1200

    Update 03_Fahrenheit_to_Celsius.py

--- 03_Fahrenheit_to_Celsius.py
+++ 03_Fahrenheit_to_Celsius.py
@@ -4,7 +4,7 @@ Function takes in a value, does the conversion and puts the answer into a list
 """
 
 
-def to_f(from_f):
+def to_c(from_f):
     celsius = (from_f - 32) * 5/9
     return celsius
 
@@ -14,7 +14,7 @@ temperatures = [0, 32, 100]
 converted = []
 
 for item in temperatures:
-    answer = to_f(item)
+    answer = to_c(item)
     ans_statement = "{} degrees F is {} degrees C".format(item, answer)
     converted.append(ans_statement)
 
