e59aea6fed909247f820af62fdb8ad7666b8fc89
Wardl1 <66809651+Wardl1@users.noreply.github.com>
2021-07-22 14:18:37 +1200

    Create 08_Data_to_txt_v1.py

---
+++ 08_Data_to_txt_v1.py
@@ -0,0 +1,17 @@
+# Source: https://www.quru99.com/reading-and-writing-files-in-python.html
+
+# Data to be outputted
+data = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh']
+
+# Get filename, can't be blank / invalid
+# assume valid data for now.
+filename = input("Enter a Filename: ")
+
+# Create file to hold data
+f = open(filename, "w+")
+
+for item in data:
+    f.write(item)
+
+# close file
+f.close()
